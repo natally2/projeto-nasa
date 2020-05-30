d7888ec29994b8f687bf63601b9ed325accb53e4
Cesar Ando <andocesar@yahoo.com.br>
2020-05-30 17:32:03 -0300

    correção tela cadastro.

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 // import Home from '../views/Home.vue'
 import Perfil from '../views/Perfil.vue'
+import Login from '../views/Login.vue'
+import Cadastro from '../views/Cadastro.vue'
 
 Vue.use(VueRouter)
 
@@ -18,7 +20,16 @@ const router = new VueRouter({
       name: 'perfil',
       component: Perfil
     },
-
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/cadastro',
+      name: 'cadastro',
+      component: Cadastro
+    },
   ]
 })
 
